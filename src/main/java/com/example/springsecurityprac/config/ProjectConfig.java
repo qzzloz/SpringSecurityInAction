2b5f77fc093ad41d04eea5cfdc17447a532bdd0e
qzzloz <xlivsxd@gmail.com>
2024-07-22 15:15:13 +0900

    ch3ex1: UserDetailsService 인터페이스, loadUserByUsername 메서드 구현

--- src/main/java/com/example/springsecurityprac/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurityprac/config/ProjectConfig.java
@@ -1,30 +1,30 @@
 package com.example.springsecurityprac.config;
 
+import com.example.springsecurityprac.services.InMemoryUserDetailsService;
+import com.example.springsecurityprac.model.User;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.List;
 
 @Configuration
 public class ProjectConfig{
 
     @Bean
     public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-        userDetailsService.createUser(user);    // UserDetailsService에서 관리하도록 사용자 추가
-
-        return userDetailsService;
+        UserDetails u = new User("john", "12345", "read");
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
     }
 
     @Bean
